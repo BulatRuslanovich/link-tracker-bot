934ab3c2ae590021a34593aeb74addde86d253d2
bulatruslanovich <bulatruslanovich@gmail.com>
2024-10-08 16:08:09 +0300

    init

---
+++ Bot/src/main/java/com/bipbup/config/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.bipbup.config;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "bot", ignoreUnknownFields = false)
+public record ApplicationConfig(
+        @NotNull
+        String token
+) {
+}
